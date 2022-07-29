e931d38ea9bfa10c1ae947c30c544c2f83cb9d05
writingdog <matrioshkadog@gmail.com>
2022-07-29 10:40:49 +0200

    Testing?

--- joy32.py
+++ joy32.py
@@ -513,7 +513,7 @@ def event_normal(e,submap,physical_btn,force_trigger=False):
                             do_hats(c,submit_value)
                         else:
                             button_map[button_invmap[c]]["s"] = 0 # New behavior. Always set this to OFF
-                        loop.call_soon_threadsafe(q.put_nowait,"{},{},{}".format("osb",button_invmap[c],-1)) # Blank the OSB
+                        loop.call_soon_threadsafe(q.put_nowait,"{},{},{}".format("osb",button_invmap[c],0)) # Blank the OSB
                     if force_delay != False:
                         #button_map[v_k]["s"] = submit_value # Set the button_map value to be either on or off, depending.
                         if submit_value == 1:
